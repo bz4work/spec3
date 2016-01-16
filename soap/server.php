f78adf338bb533cd9a13046dd165fd937e75f32a
Slimline <bz4work@gmail.com>
2016-01-16 15:10:21 +0200

    SOAP not work

--- soap/server.php
+++ soap/server.php
@@ -14,7 +14,7 @@
 	}
 	
 	//отключил в файле php.ini
-	//ini_set("soap.wsdl_cache_enabled", "0");//отключаем кэширование .wsdl файлов на время разработки
+	ini_set("soap.wsdl_cache_enabled", "0");//отключаем кэширование .wsdl файлов на время разработки
 	
 	$server = new SoapServer("http://localhost/spec3/soap/stock.wsdl");//создаем сервис (службу) и в конструктор передаем файл .wsdl с описанием сервиса
 	$server->addFunction("getStock");//добавляем функцию для нашего сервиса (службы)
