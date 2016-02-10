41a779c7ae1a771460dd6c5493db39c978b6eab6
Slimline <bz4work@gmail.com>
2016-02-10 18:05:47 +0200

    block: file parsing -> ready

--- mass_oop/index-mass-oop.php
+++ mass_oop/index-mass-oop.php
@@ -1,33 +1,90 @@
 <?php
 error_reporting(-1);
-header ('Content-type: text/html; charset=windows-1251');
+header ('Content-type: text/html; charset=utf-8');
 require "mass.class.php";
-
-$filename = '1.txt';
+?>
+<form method='post' action='index-mass-oop.php'>
+	<input type='text' name='file_name' value='<?=@$_POST['file_name']?>'> введите название файла<br>
+	<input type='text' name='code_start' value='<?=@$_POST['code_start']?>'> начать нумерацию кода товара с этой цифры <br>
+	<input type='text' name='manufac' value='<?=@$_POST['manufac']?>'> id производителя (11 - Bosch, manufactured)<br>
+	<input type='submit' name='submit' value='go'>
+</form>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+$filename = $_POST['file_name'];
+$code_start = $_POST['code_start'];
+$manufac = $_POST['manufac'];
 $test_obj = new MassInc();
 $arr = $test_obj->FileToArr($filename);
-
-
+/*
 echo '<pre>';
 print_r ($arr);
 echo '</pre>';
-
-
+*/
 
 for ($i = 0 ; $i < $arr['count']; ++$i){
-	//Почему то array_search не может найти во вложенном массиве $arr[$i] значение (низкобазовый). Разобраться.
-	if ($key = array_search('(низкобазовый)', $arr[$i])){
+	$options = array(
+					0 => 'R+',
+					1 => 'L+',
+					2 => 'JR+',
+					3 => 'JL+');
+
+	$nizbaza = $test_obj->searchArr('(низкобазовый)', $arr[$i]);
+
+	if ($nizbaza){
 		$height = '175';
 	}else{
 		$height = '190';
 	}
-	
-	?>
-	<form>
-		<input type="text" name="sizes" value="000x175x<?=$height?>">
-	</form>
-<?php 
-}
 
+	$boxType = $test_obj->searchBoxType($options, $arr[$i]);
+	$ah = $test_obj->capacityAh($arr[$i]);
+	$tok = $test_obj->startCurrent($arr[$i]);
+	$lenght = $test_obj->lenghts($arr[$i][$ah]);
+	
+	
+	if ($code_start <= 9){
+		$zero = "0000";
+	}elseif($code_start <= 99){
+		$zero = "000";
+	}elseif($code_start >= 100 && $code_start <= 1000){
+		$zero = "00";
+	}elseif ($code_start > 1000 && $code_start < 10000){
+		$zero = "0";
+	}elseif ($code_start >= 10000){
+		$zero = "";
+	}else{
+		$zero = "?";
+	}
+	/*
+	//<input type='text' name='model_<?=$i?>' value="<?=$code_zero['zero'].$code_zero['num']++?>"> код товара (model) через Класс<br>
+	*/
+?>
+	<form method='post' action='index-mass-oop.php'>
+		<input type='text' name='model_<?=$i?>' value="<?=$zero, $code_start++?>"> код товара (model)<br>
+		<input type='text' name='man_<?=$i?>' value='<?=$manufac?>'> Производитель (11 - Bosch, manufactured)<br>
+		<input type='text' name='price_<?=$i?>' value='1500'> Цена<br>
+		<input type="text" name="name_<?=$i?>" value="<?=rtrim($arr['file'][$i])?>"> H1 - Название<br>
+		<input type='text' name='mtitle_<?=$i?>' value='Аккумулятор автомобильный <?=rtrim($arr['file'][$i])?> - доставка, низкие цены, Киев и Украина' size='50'> meta-title<br>
+		<input type='text' name='mdescr_<?=$i?>' value='Аккумулятор для авто <?=rtrim($arr['file'][$i])?>. Доставка по Киеву и Укране. Скидки! Дорого купим Ваш старый АКБ.' size='50'> meta-description<br>
+		<textarea cols='52' rows='3' name='text_<?=$i?>'></textarea> Текст-описание<br>
+		<input type="text" name="size_<?=$i?>" value="<?=$lenght?>x175x<?=$height?>"> Размеры<br>
+		<input type="text" name="box_type_<?=$i?>" value="<?=$boxType['type']?>"> Тип корпуса<br>
+		<input type="text" name="polar_<?=$i?>" value="<?=$boxType['polar']?>"> Полярность<br>
+		<input type="text" name="tok_<?=$i?>" value="<?=$arr[$i][$tok]?>"> Пусковой<br>
+		<input type="text" name="ah_<?=$i?>" value="<?=$arr[$i][$ah]?>"> Емкость<br>
+
+	<hr color="red">
+<?php
+
+	}//скобка основного цикла FOR
+?>
+<input type="submit" name="submit" value="GO">
+	</form>
+<?php
+	//закрываем основой IF
+	}else{
+		echo 'загрузите файл';
+	}
 
 ?>
--- mass_oop/mass.class.php
+++ mass_oop/mass.class.php
@@ -20,13 +20,12 @@ class MassInc implements IMassInc{
 	function __destruct(){
 		unset($this->_db);
 	}
-	
 	function FileToArr($file_name){
 		$file = file("$file_name");
 		$arr['count'] = count($file);
 		$arr['file'] = $file;
-		foreach ($arr['file'] as $k=>$v){
-			$arr[] = explode(" ", $v);
+		foreach ($arr['file'] as $key => $value){
+			$arr[] = explode(" ", $value);
 		}
 		return $arr;
 	}
@@ -36,5 +35,63 @@ class MassInc implements IMassInc{
 	function IncDB($sql_inc){
 	
 	}
+	function searchArr($str, $array){
+		$key = array_search("$str", $array);
+		if ($key)
+			return true;
+		else
+			return false;
+	}
+	function searchBoxType($arrayOptions, $array){
+		if ($key = array_search("$arrayOptions[0]", $array)){
+			$res ['type'] = 'Евро';
+			$res ['polar'] = 'R+';
+		}elseif ($key = array_search("$arrayOptions[1]", $array)){
+			$res ['type'] = 'Евро';
+			$res ['polar'] = 'L+';
+		}elseif ($key = array_search("$arrayOptions[2]", $array)){
+			$res ['type'] = 'Азия';
+			$res ['polar'] = 'JR+';
+		}elseif ($key = array_search("$arrayOptions[3]", $array)){
+			$res ['type'] = 'Азия';
+			$res ['polar'] = 'JL+';
+		}else{
+			$res = '';
+		}
+		return $res;
+	}
+	function capacityAh ($array){
+		foreach ($array as $k => $v){
+			if ((int)$v > 0){
+				return $k;
+			}
+		}
+	}
+	function startCurrent ($array){
+		foreach ($array as $k => $v){
+			if ((int)$v > 250){
+				return $k;
+			}
+		}
+	}
+	function lenghts($capacity){
+		$capacity = (int)$capacity;
+		if ($capacity < 52){
+			return $lenght = '205';
+		}elseif ($capacity > 50 && $capacity < 70){
+			return $lenght = '242';
+		}elseif ($capacity > 60 && $capacity < 80){
+			return $lenght = '276';
+		}elseif ($capacity > 79 && $capacity < 86){
+			return $lenght = '315';
+		}elseif ($capacity > 85 && $capacity < 106){
+			return $lenght = '353';
+		}elseif ($capacity > 105 && $capacity < 120){
+			return $lenght = '393';
+		}else{
+			return $lenght = '000';
+		}
+	}
+
 }//закрывающая скобка класса
 ?>
