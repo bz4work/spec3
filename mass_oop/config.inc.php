8d87384d9aa01e7b7a242225918be33809b0dab8
Slimline <bz4work@gmail.com>
2016-03-19 13:25:30 +0200

    Forse залиты на сайт

--- mass_oop/config.inc.php
+++ mass_oop/config.inc.php
@@ -4,9 +4,10 @@ define ('DB_USER', '');
 define ('DB_PASS', '');
 define ('DB_NAME', '');
 
-$filename = "./goods/amega-truck.txt";
+$filename = "./goods/forse-truck.txt";
 
-$code_start = 235;
+$code_start = 262;
+
 //Bosch - 11
 //Varta - 12
 //Westa - 13
@@ -15,7 +16,8 @@ $code_start = 235;
 //Berga - 16
 //Mutlu - 17
 //A-mega - 14
-$manufac = 14;
+//Forse - 18
+$manufac = 18;
 
 //Основная категория товара
 //Аккумуляторы для легковых - 59
@@ -32,5 +34,7 @@ define ('CAT_ID', 60);
 //Mutlu - 65
 //A-mega - 70
 //A-mega грузовые - 77
-define ('CAT_ID_2', 77);
+//Forse - 68
+//Forse грузовые - 78
+define ('CAT_ID_2', 78);
 ?>
